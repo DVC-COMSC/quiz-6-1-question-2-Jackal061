aacc8eb980ae9fdea01f87b435d27016aadfc46a
Jackal061 <jordanmacle2005@gmail.com>
2024-03-15 22:07:52 -0700

    commit1

--- main.hpp
+++ main.hpp
@@ -1,3 +1,35 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void writeFile(string&);
+
+void writeFile(string& filename){
+    int N;
+    int employeeid;
+    string department;
+    string employeename;
+    int salary;
+    int N;
+    ofstream ofs;
+
+
+    ofs.open("employee.txt");
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    cout << "amount of employees: ";
+    cin >> N;
+    ofs << N << endl;
+
+    for (int i = 0; i < N; i++){
+        cout << "Enter the id, name, department, and salary of the employee: " << endl;
+        cin >> employeeid >> employeename >> department >> salary;
+        ofs << employeeid << "\t" << employeename << "\t" << department << "\t" << salary << endl;
+    }
+
+    ofs.close();
+}
